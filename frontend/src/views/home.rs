45250219c27e64f81291ddcca80536d9873a8a01
pedromarquetti <pedro.marquettti@gmail.com>
2023-10-16 23:29:56 -0300

    variable renaming

--- frontend/src/views/home.rs
+++ frontend/src/views/home.rs
@@ -16,12 +16,13 @@ pub fn home() -> Html {
     let recipe_search = use_node_ref();
     let navigator = use_navigator().unwrap();
 
+    // submit handling
     let onsubmit = {
-        let recipe_search = recipe_search.clone();
+        let input_node_ref = recipe_search.clone();
 
         Callback::from(move |event: SubmitEvent| {
             event.prevent_default();
-            let search = recipe_search.cast::<HtmlInputElement>().unwrap().value();
+            let search = input_node_ref.cast::<HtmlInputElement>().unwrap().value();
             navigator.push(&Route::RecipeList { name: search })
         })
     };
@@ -29,15 +30,18 @@ pub fn home() -> Html {
     html! {
         <div class="home-page">
             <h1>{"Welcome to my recipes app"}</h1>
-            <form {onsubmit}>
-            <Input
-            input_node_ref={recipe_search}
-            input_placeholder="type search string"
-            input_name="search"
-            input_type={InputType::Text}
-            />
-            <button>{"search recipes"}</button>
-            </form>
+
+            <div class="search-recipe">
+                <form {onsubmit}>
+                    <Input
+                    input_node_ref={recipe_search}
+                    input_placeholder="type search string"
+                    input_name="search"
+                    input_type={InputType::Text}
+                    />
+                    <button>{"search recipes"}</button>
+                </form>
+            </div>
         </div>
     }
 }
