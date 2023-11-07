07f44644b770925b4c78fa68182de7448261e0b5
pedromarquetti <pedro.marquettti@gmail.com>
2023-11-06 22:27:32 -0300

    reimplement "go home"

--- frontend/src/components/navbar_component.rs
+++ frontend/src/components/navbar_component.rs
@@ -1,13 +1,15 @@
-use crate::components::go_home_component::GoHome;
+use crate::views::Route;
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 #[function_component(NavBar)]
 pub fn navbar() -> Html {
     html! {
         <nav class="top-nav">
-        <GoHome/>
+            <Link<Route> to={Route::Home} >{"Home"}</ Link<Route>>
 
 
+            <Link<Route> to={Route::NewRecipe}>{"New Recipe"}</Link<Route>>
         <p>{"todo: implement user login"}</p>
         </nav>
     }
