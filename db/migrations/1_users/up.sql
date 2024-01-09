6434f5713775daceddd721683ecb9653592cc1b4
pedromarquetti <pedro.marquettti@gmail.com>
2024-01-08 23:06:32 -0300

    create default admin user with admin hashed password

--- db/migrations/1_users/up.sql
+++ db/migrations/1_users/up.sql
@@ -4,3 +4,11 @@ CREATE TABLE IF NOT EXISTS recipe_users (
     user_pwd TEXT NOT NULL,
     user_role TEXT NOT NULL
 );
+/* creating default admin user */
+INSERT INTO recipe_users (id, user_name, user_pwd, user_role)
+VALUES (
+    0,
+    'admin',
+    '$2a$04$Plkgo7MNUzowxl8/r0tFMeQE2E8DDej6oKkuqXMr5EIUcZKTSGGIS',
+    'admin'
+) ON CONFLICT DO NOTHING;
