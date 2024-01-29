c1897c6a0e01412e1ad1c3e620898f246f2eb436
pedromarquetti <pedro.marquettti@gmail.com>
2024-01-28 21:23:43 -0300

    make connect to db func. more generic

--- db/src/db_pool.rs
+++ db/src/db_pool.rs
@@ -1,3 +1,4 @@
+use std::fmt::Display;
 use std::time::Duration;
 
 use diesel::r2d2::PooledConnection;
@@ -12,8 +13,8 @@ pub type Pool = R2D2Pool<ConnectionManager<PgConnection>>;
 pub type DbConnection = Result<PooledConnection<ConnectionManager<PgConnection>>, R2D2Err>;
 pub type PooledPgConnection = PooledConnection<ConnectionManager<PgConnection>>;
 
-pub fn connect_to_db(url: String) -> Result<Pool, R2D2Err> {
-    let manager = ConnectionManager::<PgConnection>::new(url);
+pub fn connect_to_db<S: Display>(url: S) -> Result<Pool, R2D2Err> {
+    let manager = ConnectionManager::<PgConnection>::new(url.to_string());
     Pool::builder()
         .connection_timeout(Duration::from_secs(1))
         .build(manager)
