d4d1d48e0da504e8aaa29cb41c1a01857138c0cf
pedromarquetti <pedro.marquettti@gmail.com>
2024-07-25 10:14:33 -0300

    New tests implemented for DB

--- db/src/tests.rs
+++ db/src/tests.rs
@@ -1,7 +1,10 @@
+use crate::functions::recipe::{
+    create_recipe_query, delete_recipe_query, fuzzy_query, update_recipe_query,
+};
 use crate::functions::user::{
     create_user_record, delete_user_record, list_users_query, query_user_info, update_user_record,
 };
-use crate::structs::UrlUserQuery;
+use crate::structs::{FullRecipe, Ingredient, Recipe, Step, UrlRecipeQuery, UrlUserQuery};
 use diesel::result::Error;
 use diesel::Connection;
 use std::env;
@@ -145,4 +148,171 @@ fn test_list_users_query() {
         assert!(new_len.len() > old_len);
         Ok(())
     });
+}
+
+#[test]
+fn create_recipe() {
+    let pool = connect_to_db(get_db_url()).expect("failed to get pool");
+    let mut conn = pool.get().expect("failed to get connection from pool");
+    let input_recipe = Recipe::default();
+    conn.test_transaction(|conn| {
+        create_recipe_query(conn, &input_recipe)?;
+        Ok::<_, DieselError>(())
+    })
+}
+
+#[test]
+fn test_delete_recipe() {
+    let pool = connect_to_db(get_db_url()).expect("failed to get pool");
+    let mut conn = pool.get().expect("failed to get connection from pool");
+    let first_recipe = Recipe {
+        recipe_name: String::from("value"),
+        recipe_observations: None,
+        ..Default::default()
+    };
+    let second_recipe = Recipe {
+        id: Some(12),
+        recipe_observations: None,
+        ..Default::default()
+    };
+    conn.test_transaction::<_, DieselError, _>(|conn| {
+        create_recipe_query(conn, &first_recipe)?;
+        create_recipe_query(conn, &second_recipe)?;
+        let old_len = fuzzy_query(conn, &String::from(""))?.len();
+        println!("recipe table length after create_recipe {old_len}");
+        delete_recipe_query(
+            conn,
+            &UrlRecipeQuery {
+                id: second_recipe.id,
+                name: None,
+            },
+        )?;
+        assert!(
+            fuzzy_query(conn, &String::from(""))?.len() == old_len - 1,
+            "recipe 1 wasn't deleted!"
+        );
+        delete_recipe_query(
+            conn,
+            &UrlRecipeQuery {
+                id: None,
+                name: Some(first_recipe.recipe_name),
+            },
+        )?;
+        assert!(
+            fuzzy_query(conn, &String::from(""))?.len() == old_len - 2,
+            "recipe 2 wasn't deleted!"
+        );
+
+        Ok(())
+    })
+}
+
+#[test]
+fn test_fuzzy_query() {
+    let pool = connect_to_db(get_db_url()).expect("failed to get pool");
+    let mut conn = pool.get().expect("failed to get connection from pool");
+    let recipe = Recipe {
+        recipe_name: "test_fuzzy_query".to_string(),
+        ..Default::default()
+    };
+    conn.test_transaction::<_, DieselError, _>(move |conn| {
+        create_recipe_query(conn, &recipe)?;
+        let recipes = fuzzy_query(conn, &String::from("test_fuzzy_query"))?;
+        assert_eq!(recipes.len(), 1, "Recipe not found");
+
+        Ok(())
+    })
+}
+
+#[test]
+fn test_update_recipe() {
+    let pool = connect_to_db(get_db_url()).expect("failed to get pool");
+    let mut conn = pool.get().expect("failed to get connection from pool");
+    let old_recipe = Recipe {
+        id: Some(32),
+        recipe_name: "pao".to_string(),
+        ..Default::default()
+    };
+    conn.test_transaction::<_, DieselError, _>(move |conn| {
+        let created = create_recipe_query(conn, &old_recipe)?;
+
+        let new_recipe = Recipe {
+            id: old_recipe.id,
+            recipe_name: String::from("tijolo"),
+            ..old_recipe
+        };
+        let updated = update_recipe_query(conn, &new_recipe)?;
+        assert_eq!(created.id, updated.id);
+        assert_ne!(created.recipe_name, updated.recipe_name);
+        Ok(())
+    })
+}
+
+#[test]
+fn test_fullrecipe_helpers() {
+    let ingredients = vec![
+        Ingredient {
+            id: Some(10),
+            ..Default::default()
+        },
+        Ingredient {
+            id: Some(12),
+            ..Default::default()
+        },
+        Ingredient {
+            id: Some(11),
+            ..Default::default()
+        },
+    ];
+    let original_ingredients_len = ingredients.len();
+    let steps = vec![
+        Step {
+            id: Some(10),
+            ..Default::default()
+        },
+        Step {
+            id: Some(12),
+            ..Default::default()
+        },
+        Step {
+            id: Some(11),
+            ..Default::default()
+        },
+    ];
+    let original_steps_len = steps.len();
+    let mut full_recipe = FullRecipe {
+        ingredients,
+        steps,
+        ..Default::default()
+    };
+
+    let new_i_len = full_recipe
+        .remove_ingredient(10)
+        .map_err(|err| panic!("{}", err))
+        .unwrap();
+    let new_s_len = full_recipe
+        .remove_step(10)
+        .map_err(|err| panic!("{}", err))
+        .unwrap();
+    assert!(new_s_len.len() == original_steps_len - 1);
+    assert!(new_i_len.len() == original_ingredients_len - 1);
+
+    let old_ingredient = full_recipe
+        .get_ingredient(11)
+        .map_err(|err| panic!("{}", err))
+        .unwrap();
+    let i = full_recipe
+        .replace_ingredient(Ingredient {
+            id: Some(11),
+            ingredient_name: "new_name".to_string(),
+            ..Default::default()
+        })
+        .map_err(|err| panic!("error: {err}"))
+        .unwrap();
+    full_recipe.set_ingredients(i);
+    let updated_ingredient = full_recipe
+        .get_ingredient(11)
+        .map_err(|err| panic!("error! {err}"))
+        .unwrap();
+    assert_ne!(old_ingredient, updated_ingredient)
 }
