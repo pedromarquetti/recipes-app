d7bd3c94827291a3025ba429a0b24696aa1f4f41
pedromarquetti <pedro.marquettti@gmail.com>
2024-07-25 21:15:54 -0300

    add special structs for handling inserts on the db

--- db/src/structs.rs
+++ db/src/structs.rs
@@ -16,23 +16,17 @@ use diesel::{
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-/// This struct represents possible Ok Values the API can generate
-pub struct ApiOkResponse {
-    pub msg: String,
-}
+pub trait RecipeTrait {}
+impl RecipeTrait for FullRecipe {}
+impl RecipeTrait for Recipe {}
+impl RecipeTrait for Ingredient {}
+impl RecipeTrait for Step {}
+impl<T> RecipeTrait for Vec<T> {}
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 #[cfg_attr(
     not(target_arch = "wasm32"),
-    derive(
-        Queryable,
-        Selectable,
-        AsChangeset,
-        Identifiable,
-        Associations,
-        Insertable,
-    )
+    derive(Queryable, Selectable, AsChangeset, Identifiable, Associations,)
 )]
 #[cfg_attr(not(target_arch = "wasm32"), 
     diesel(belongs_to(Recipe)),
@@ -65,14 +59,33 @@ impl Step {
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 #[cfg_attr(
     not(target_arch = "wasm32"),
-    derive(
-        Queryable,
-        Selectable,
-        AsChangeset,
-        Identifiable,
-        Associations,
-        Insertable
-    )
+    derive(AsChangeset, Associations, Insertable,)
+)]
+#[cfg_attr(not(target_arch = "wasm32"), 
+    diesel(belongs_to(Recipe)),
+    diesel(table_name = recipe_step),
+)]
+pub struct NewStep {
+    pub recipe_id: i32,
+    pub step_name: String,
+    pub step_instruction: String,
+    pub step_duration_min: i32,
+}
+impl Default for NewStep {
+    fn default() -> Self {
+        Self {
+            recipe_id: -1,
+            step_name: String::new(),
+            step_instruction: String::new(),
+            step_duration_min: -1,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[cfg_attr(
+    not(target_arch = "wasm32"),
+    derive(Queryable, Selectable, AsChangeset, Identifiable, Associations,)
 )]
 #[cfg_attr(not(target_arch="wasm32"), 
     diesel(belongs_to(Recipe)),
@@ -102,19 +115,38 @@ impl Ingredient {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[cfg_attr(
+    not(target_arch = "wasm32"),
+    derive(AsChangeset, Associations, Insertable)
+)]
+#[cfg_attr(not(target_arch="wasm32"), 
+    diesel(belongs_to(Recipe)),
+    diesel(table_name = recipe_ingredient),
+)]
+pub struct NewIngredient {
+    pub recipe_id: i32,
+    pub ingredient_name: String,
+    pub ingredient_quantity: i32,
+    pub quantity_unit: String,
+}
+impl Default for NewIngredient {
+    fn default() -> Self {
+        Self {
+            recipe_id: -1,
+            ingredient_name: String::new(),
+            ingredient_quantity: -1,
+            quantity_unit: String::new(),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 // configuring attributes
 // if target_arch (architeture the code is being compiled in) is wasm32, ignore these (diesel stuff)
 #[cfg_attr(
     not(target_arch = "wasm32"),
-    derive(
-        AsChangeset,
-        Queryable,
-        Selectable,
-        Associations,
-        Insertable,
-        Identifiable,
-    )
+    derive(AsChangeset, Queryable, Selectable, Associations, Identifiable,)
 )]
 #[cfg_attr(not(target_arch = "wasm32"), 
     diesel(belongs_to(User)),
@@ -122,7 +154,7 @@ impl Ingredient {
 )]
 pub struct Recipe {
     pub id: i32,
-    pub user_id: Option<i32>,
+    pub user_id: i32,
     pub recipe_name: String,
     pub recipe_observations: Option<Vec<Option<String>>>,
 }
@@ -130,7 +162,7 @@ impl Default for Recipe {
     fn default() -> Self {
         Recipe {
             id: 0,
-            user_id: None,
+            user_id: 0,
             recipe_name: String::new(),
             recipe_observations: None,
         }
@@ -148,7 +180,7 @@ impl Recipe {
         self.id = recipe_id
     }
     pub fn set_user_id(&mut self, user_id: i32) {
-        self.user_id = Some(user_id)
+        self.user_id = user_id
     }
 
     pub fn set_observation(&mut self, obs: Option<Vec<Option<String>>>) {
@@ -156,24 +188,28 @@ impl Recipe {
     }
 }
 
-pub trait RecipeTrait {}
-impl RecipeTrait for FullRecipe {}
-impl RecipeTrait for Recipe {}
-impl RecipeTrait for Ingredient {}
-impl RecipeTrait for Step {}
-impl<T> RecipeTrait for Vec<T> {}
-
-#[derive(Deserialize, Serialize, Debug, PartialEq)]
-pub struct UrlRecipeQuery {
-    pub id: Option<i32>,
-    pub name: Option<String>,
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+// configuring attributes
+// if target_arch (architeture the code is being compiled in) is wasm32, ignore these (diesel stuff)
+#[cfg_attr(
+    not(target_arch = "wasm32"),
+    derive(AsChangeset, Queryable, Selectable, Insertable)
+)]
+#[cfg_attr(not(target_arch = "wasm32"), 
+    diesel(belongs_to(User)),
+    diesel(table_name = recipe),
+)]
+pub struct NewRecipe {
+    pub user_id: i32,
+    pub recipe_name: String,
+    pub recipe_observations: Option<Vec<Option<String>>>,
 }
-
-impl Default for UrlRecipeQuery {
+impl Default for NewRecipe {
     fn default() -> Self {
-        UrlRecipeQuery {
-            id: None,
-            name: None,
+        Self {
+            user_id: 0,
+            recipe_name: String::new(),
+            recipe_observations: None,
         }
     }
 }
@@ -341,6 +377,63 @@ impl FullRecipe {
     }
 }
 
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    not(target_arch = "wasm32"),
+    derive(AsChangeset, Queryable, Selectable, Insertable)
+)]
+#[cfg_attr(not(target_arch="wasm32"), 
+    diesel(table_name = recipe_users)
+)]
+pub struct NewUser {
+    pub user_name: String,
+    pub user_role: UserRole,
+    pub user_pwd: String,
+}
+impl Default for NewUser {
+    fn default() -> Self {
+        Self {
+            user_name: String::new(),
+            user_role: UserRole::User,
+            user_pwd: String::new(),
+        }
+    }
+}
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    not(target_arch = "wasm32"),
+    derive(AsChangeset, Queryable, Selectable, Identifiable,)
+)]
+#[cfg_attr(not(target_arch="wasm32"), 
+    diesel(table_name = recipe_users)
+)]
+pub struct User {
+    pub id: i32,
+    pub user_name: String,
+    pub user_role: UserRole,
+    pub user_pwd: String,
+}
+impl Default for User {
+    fn default() -> Self {
+        User {
+            id: 0,
+            user_name: String::new(),
+            user_role: UserRole::User,
+            user_pwd: String::new(),
+        }
+    }
+}
+impl User {
+    /// modify Steps inside FullRecipe
+    pub fn set_id(&mut self, user_id: i32) {
+        self.id = user_id
+    }
+    /// Password validation function
+    pub fn validate(&self, pwd: &str) -> Result<String, IOError> {
+        return Ok(pwd.into());
+    }
+}
+
 #[derive(Deserialize, Clone, Debug, Copy, PartialEq, Eq, Serialize)]
 #[cfg_attr(not(target_arch = "wasm32"), derive(FromSqlRow, AsExpression))]
 #[cfg_attr(not(target_arch = "wasm32"), diesel(sql_type = Text))
@@ -381,45 +474,18 @@ impl FromSql<Text, Pg> for UserRole {
     }
 }
 
-#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
-#[cfg_attr(not(target_arch="wasm32"), 
-    // derive( Queryable, Selectable, Insertable, Identifiable),
-    derive(
-        AsChangeset,
-        Queryable,
-        Selectable,
-        Insertable,
-        Identifiable,
-    ),
-)]
-#[cfg_attr(not(target_arch="wasm32"), 
-    diesel(table_name = recipe_users)
-)]
-pub struct User {
-    pub id: i32,
-    pub user_name: String,
-    pub user_role: UserRole,
-    pub user_pwd: String,
-}
-impl Default for User {
-    fn default() -> Self {
-        User {
-            id: 0,
-            user_name: String::new(),
-            user_role: UserRole::User,
-            user_pwd: String::new(),
-        }
-    }
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+pub struct UrlRecipeQuery {
+    pub id: Option<i32>,
+    pub name: Option<String>,
 }
 
-impl User {
-    /// modify Steps inside FullRecipe
-    pub fn set_id(&mut self, user_id: i32) {
-        self.id = user_id
-    }
-    /// Password validation function
-    pub fn validate(&self, pwd: &str) -> Result<String, IOError> {
-        return Ok(pwd.into());
+impl Default for UrlRecipeQuery {
+    fn default() -> Self {
+        UrlRecipeQuery {
+            id: None,
+            name: None,
+        }
     }
 }
 
@@ -436,4 +502,10 @@ impl Default for UrlUserQuery {
             name: None,
         }
     }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+/// This struct represents possible Ok Values the API can generate
+pub struct ApiOkResponse {
+    pub msg: String,
 }
